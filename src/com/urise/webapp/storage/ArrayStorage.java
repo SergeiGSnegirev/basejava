539c7b75cff436d8576344b635dc0e0f0ee964bd
Sergei Snegirev <snegirev@jet.su>
2025-02-23 18:40:44 +0300

    add update(); add getResumePosition(); optimize get, delete, save, update by using getResumePosition(); optimize clear(), getAll() by using java.util.Arrays methods.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,32 +2,64 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private final int STORAGE_SIZE = 10000;
+    private Resume[] storage = new Resume[STORAGE_SIZE];
     private int size = 0;
+
+    /**
+     * @return resume's position in storage by its uuid or -1 if it is not found
+     */
+    private int getResumePosition(String uuid, boolean warning) {
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(storage[i].getUuid(), uuid)) {
+                return i;
+            }
+        }
+        if (warning) {
+            System.out.printf("ОШИБКА: Резюме с uuid = '%s' нет в storage!\n", uuid);
+        }
+        return -1;
+    }
 
     /**
      * Deletes all resumes in storage
      */
     public void clear() {
-        for (int i = 0; i < size; i++) storage[i] = null;
+        Arrays.fill(storage, 0, size, null);
         size = 0;
+    }
+
+    /**
+     * Updates resume in storage
+     */
+    public void update(Resume r) {
+        int i = getResumePosition(r.getUuid(), true);
+        if (i != -1) {
+            // stub, TBD what to update
+            storage[i] = r;
+        }
     }
 
     /**
      * Appends resume to the end of Resumes storage
      */
     public void save(Resume r) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(storage[i].getUuid(), r.getUuid())) {
-                System.out.printf("Резюме с uuid = '%s' уже имеется в storage. Введите уникальное значение uuid!\n", storage[i].getUuid());
-                return;
-            }
+        int i = getResumePosition(r.getUuid(), false);
+        if (i != -1) {
+            System.out.printf("ОШИБКА: Резюме с uuid = '%s' уже имеется в storage. ", storage[i].getUuid());
+            System.out.println("Введите уникальное значение uuid!");
+            return;
+        }
+        if (size == STORAGE_SIZE) {
+            System.out.printf("ОШИБКА: storage полностью заполнено, хранит %s резюме.\n", STORAGE_SIZE);
+            return;
         }
         storage[size] = r;
         size++;
@@ -37,8 +69,9 @@ public class ArrayStorage {
      * @return resume by uuid
      */
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(storage[i].getUuid(), uuid)) return storage[i];
+        int i = getResumePosition(uuid, true);
+        if (i != -1) {
+            return storage[i];
         }
         return null;
     }
@@ -47,13 +80,11 @@ public class ArrayStorage {
      * Deletes resume by its uuid by replacing it by the last one
      */
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (Objects.equals(storage[i].getUuid(), uuid)) {
-                storage[i] = storage[size - 1];
-                storage[size - 1] = null;
-                size--;
-                break;
-            }
+        int i = getResumePosition(uuid, true);
+        if (i != -1) {
+            storage[i] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
         }
     }
 
@@ -61,9 +92,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] resumes = new Resume[size];
-        System.arraycopy(storage, 0, resumes, 0, size);
-        return resumes;
+        return Arrays.copyOf(storage, size);
     }
 
     /**

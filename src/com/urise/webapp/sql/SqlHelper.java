1db935f8a649a779f5f1197b03fd40ba7a109810
Sergei Snegirev <snegirev@jet.su>
2025-05-09 21:41:49 +0300

    Lesson14: introduce SqlTransaction, ExceptionUtil

--- src/com/urise/webapp/sql/SqlHelper.java
+++ src/com/urise/webapp/sql/SqlHelper.java
@@ -1,6 +1,5 @@
 package com.urise.webapp.sql;
 
-import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.StorageException;
 
 import java.sql.Connection;
@@ -20,10 +19,22 @@ public class SqlHelper {
              PreparedStatement ps = conn.prepareStatement(sql)) {
             return executor.execute(ps);
         } catch (SQLException e) {
-            if (e.getSQLState().equals("23505")) {
-                throw new ExistStorageException(sql);
+            throw ExceptionUtil.convertException(e);
+        }
+    }
+
+    public <T> T transactionalExecute(SqlTransaction<T> executor) {
+        try (Connection conn = connectionFactory.getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+                T result = executor.execute(conn);
+                conn.commit();
+                return result;
+            } catch (SQLException e) {
+                conn.rollback();
+                throw ExceptionUtil.convertException(e);
             }
-//                System.out.println("getSQLState = " + e.getSQLState());
+        } catch (SQLException e) {
             throw new StorageException(e);
         }
     }
